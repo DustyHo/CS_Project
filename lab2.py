f054cdd8d2c3c8472595c878e225cc3a88a1792d
DustyHo <130243098+DustyHo@users.noreply.github.com>
2023-04-08 15:50:25 -0400

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,13 +1,1 @@
-def dictionary1(words):
-    dic = {}
-    for word in words:
-        if word in dic:
-            dic[word] = dic[word] + 1
-        else:
-            dic[word] = 1
-    return dic
-words = input().split()
-dictionary = dictionary1(words)
-keys = sorted(dictionary.keys())
-for key in keys:
-    print(key + ': ' + str(dictionary[key]))
+Print('Hello')
